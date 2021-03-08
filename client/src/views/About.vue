248cf14a53c85388ad9b3ac9833365244f154933
Yash <pirsquare.yash@gmail.com>
2021-03-08 17:55:13 +0800

    Oauth login complete

--- client/src/views/About.vue
+++ client/src/views/About.vue
@@ -1,5 +1,19 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <div class="about">
+        <button @click="testing">Test</button>
+    </div>
 </template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+    name: "Home",
+    methods: {
+        testing() {
+            this.$socket.client.emit("testing", "Test string");
+        },
+    },
+};
+</script>
