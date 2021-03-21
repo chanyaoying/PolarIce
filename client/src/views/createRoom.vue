b039dc1beb0a42a1628e4391e0af1283b9c56745
jiali0331 <72539000+jiali0331@users.noreply.github.com>
2021-03-21 14:06:31 +0800

    UI

--- client/src/views/createRoom.vue
+++ client/src/views/createRoom.vue
@@ -1,13 +1,122 @@
 <template>
-    <h1> Hello, Kenny! </h1>
+<div class="room">
+    <!-- do v-if="roomID.length < 0", show modal button. 
+    Generae random roomID, add to roomID, show create Question page-->
+    <div v-if="roomID.length <= 0">
+        <b-button variant="warning" v-b-modal.modal-1 @click="generateRoom()">Create New Room</b-button>
+    </div>
+
+    <div class="card" style="margin:20px auto; width:60%;" v-else>
+        <div class="card-body">
+            <h1>RoomID: {{roomID}}</h1>
+            <b-button style="margin:20px;" variant="success" @click="createQuestion()">Create My Own Question</b-button>
+            <b-button style="margin:20px;" variant="warning" @click="randomQuestion()">Generate Random Question</b-button>
+            
+            <!-- Random questions -->
+            <div v-if="!createQ && randomQ">
+                <ol>
+                    <li style="text-align:left;" v-for="selfquestion in questions" :key="selfquestion">
+                        <span><b>Question:</b> {{selfquestion.question}}</span><br>
+                        <span><b>Choices:</b> {{selfquestion.choice}}</span><hr>
+                    </li>
+                
+                </ol>
+            </div>
+            
+
+            <!-- Create own question -->
+            <div v-if="createQ && !randomQ">
+                <b-form>
+                <b-form-group id="input-group-1" label="Add New Question:" label-for="input-1">
+                    <b-form-input id="input-1" v-model="newQ.question" placeholder="Enter your question." required></b-form-input>
+                    <b-form-input id="input-1" v-model="newQ.choice1" placeholder="Enter choice 1" required></b-form-input>
+                    <b-form-input id="input-1" v-model="newQ.choice2" placeholder="Enter choice 2" required></b-form-input>
+                </b-form-group>
+
+                <b-button variant="primary" @click="onSubmit">Submit</b-button>
+                <b-button variant="danger" @click="onReset">Reset</b-button>
+                </b-form>
+
+                <b-card class="mt-3" header="Question">
+                    <ol>
+                        <li style="text-align:left;" v-for="question in selfQuestion" :key="question">
+                            <span><b>Question:</b> {{question.question}}</span><br>
+                            <span><b>Choices:</b> {{question.choice}}</span><hr>
+                        </li>
+                    </ol>
+                </b-card>
+            </div>
+
+        </div>
+    </div>
+</div>
+    
 </template>
 
 <script>
 export default {
+    // name: 'room',
+    data: () => ({
+        newQ:{
+            question: "",
+            choice1: "",
+            choice2:""
+        },
+        randomQ: true,
+        createQ: false
+    }),
+    methods: {
+        generateRoom(){
+            return this.$store.state.roomID = (Math.floor(Math.random()*1000000));
+        },
+        onSubmit(){
+            this.$store.state.selfQuestion.push({
+                question:this.newQ.question,
+                choice:[this.newQ.choice1,this.newQ.choice2]
+                });
+            this.newQ.question = '';
+            this.newQ.choice1 = '';
+            this.newQ.choice2 = '';
+        },
+        onReset(){
+            this.newQ.question = '';
+            this.newQ.choice1 = '';
+            this.newQ.choice2 = '';
+        },
+        randomQuestion(){
+            this.randomQ = true;
+            this.createQ = false;
+        },
+        createQuestion(){
+            this.createQ = true;
+            this.randomQ = false;
+        }
+    },
+    
+    computed:{
+        roomID(){
+            return this.$store.state.roomID;
+        },
+        questions(){
+            return this.$store.state.questions;
+        },
+        selfQuestion(){
+            return this.$store.state.selfQuestion;
+        }
+        
+    }
 
 }
+
 </script>
 
 <style>
-
+b-button{
+    padding:20px;
+}
 </style>
+
+
+
+
+/
--- client/src/views/manageRoom.vue
+++ client/src/views/manageRoom.vue
@@ -1,12 +1,83 @@
 <template>
     <div>
-        <button @click="createRoom()">Create Room</button>
-        <!-- <h1>Welcome, {{ userData.name }}</h1> 
-        <p>{{ userData.email }}</p>
-        <img :src="userData.profile_pic" alt="" /> -->
-    </div>
-    
+        <br><br>
+            <b-card-group deck id="card-group">
+                <b-card>
+                <b-card-header>
+                    Welcome, {{ userData.name }}
+                </b-card-header>
+                <b-card-text id="room1">Let's get started:</b-card-text>
+                <b-row>
+                <b-card id = "card">
+                    <b-avatar :src="require('../assets/game.png')" size="6rem"></b-avatar>
+                    <b-button href="#" variant="dark" id="button">Play Game</b-button>
+                </b-card>
+                <b-card id = "card">
+                    <b-avatar :src="require('../assets/create.png')" size="6rem"></b-avatar>
+                    <b-button href="#" variant="dark" id="button">Create Room</b-button>
+                </b-card>
+                <b-card id = "card">
+                    <b-avatar :src="require('../assets/edit.png')" size="6rem"></b-avatar>
+                    <b-button href="#" variant="dark" id="button">Edit Room</b-button>
+                </b-card>
+                </b-row>
+                </b-card>
+            </b-card-group>
+        <br><br>
+
+        <b-card-group deck id="card-group">
+            <b-card
+                header="Rooms"
+                header-tag="header"
+                footer="PolarIce"
+                footer-tag="footer"              
+            >
+            <b-col>
+                <b-link id="link" href="#foo">Room: ESD Room 1</b-link>
+            </b-col>
+            <b-col>
+                <b-link id="link" href="#foo">Room: ESD Room 2</b-link>
+            </b-col>
+            <b-col>
+                <b-link id="link" href="#foo">Room: ESD Room 3</b-link>
+            </b-col>
+            <b-col id="col">
+                <b-link id="link" href="#foo">Room: ESD Room 4</b-link>
+            </b-col>      
+            </b-card>
+        </b-card-group>
+        <br><br>
+    </div>    
 </template>
+
+<style scoped>
+    #card-group{
+        width:50%;
+        margin: auto;
+    }
+    #button{
+        margin-top:10px
+    }
+    #room{
+        font-size: large;
+    }
+    #card{
+        width: 30%;
+    }
+    #room1{
+        text-align: left;
+        font-size: large;
+    }
+    #create1{
+        margin-right:10px;
+    }
+    #col{
+        margin-bottom:20px;
+    }
+    #link{
+        font-size:20px;
+    }
+</style>
 
 <script>
 import authAxios from '../components/authAxios.js';
