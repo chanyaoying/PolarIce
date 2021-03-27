181ec80b058a03cc53791e26b7e41cd04781b7f4
jiali0331 <72539000+jiali0331@users.noreply.github.com>
2021-03-28 03:54:41 +0800

    ..

--- client/src/views/404.vue
+++ client/src/views/404.vue
@@ -4,26 +4,5 @@
         <p>To play a game, the URL should be <em>playGame/roomID.</em></p>
         <p>For now, only "testRoom" is available.</p>
 
-    <div>
-        <span
-        @mouseover="hover = true"
-        @mouseleave="hover = false"
-        >
-        Hover me to show the message!
-        </span>
-        <span v-if="hover">
-            <br>
-            <img id ="animation" src="../assets/animation.gif" />
-        </span>
-   </div>
     </div>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      hover: false,
-    };
-  }
-}
-</script>
