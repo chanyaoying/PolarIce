53f1244d46d8e8c10568ff9d240d68747fe9d888
suicidal-muffin <62653896+suicidal-muffin@users.noreply.github.com>
2021-04-07 06:55:03 +0800

    Added 30 seconds anti-spam ban

--- client/src/components/gameComponents/chatBox.vue
+++ client/src/components/gameComponents/chatBox.vue
@@ -1,36 +1,53 @@
 <template>
 	<div id="right">
 		<b-card>
-		<h2>Message</h2>
-		<br />
-		<div class="scroll">
-			<ul style="list-style-type:none;">
-				<li v-for="(data, key) in chatHistory[roomID]" :key="key" style="text-align: left;">
-					<div class="profword" v-if="data[0] == ':'">
-						{{ profMessage(data) }}
-					</div>
-					<div v-else>
-						{{ data }}
-					</div>
-				</li>
-			</ul>
-		</div>
-		Send a message to the room: <br />
-	
-		<b-input-group id="message" class="mt-3">
-			<b-form-input
-				type="text"
-				v-model="message"
-				@keyup.enter="sendMessage"
-				placeholder="Enter message"
-			></b-form-input>
-			<b-input-group-append>
-				<b-button @click="sendMessage" variant="dark">
-					Send Message
-				</b-button>
-			</b-input-group-append>
-		</b-input-group>
-		<br />
+			<h2>Message</h2>
+			<p v-if="banned" id="banned">
+				You have been banned from chatting for 30 seconds.
+			</p>
+			<br />
+			<div class="scroll">
+				<ul style="list-style-type: none">
+					<li
+						v-for="(data, key) in chatHistory[roomID]"
+						:key="key"
+						style="text-align: left"
+					>
+						<div class="profword" v-if="data[0] == ':'">
+							{{ systemMessage(data) }}
+						</div>
+						<div v-else>
+							{{ data }}
+						</div>
+					</li>
+				</ul>
+			</div>
+			Send a message to the room: <br />
+
+			<b-input-group id="message" class="mt-3">
+				<b-form-input
+					type="text"
+					v-model="message"
+					@keyup.enter="sendMessage"
+					placeholder="Enter message"
+					v-if="!banned"
+				></b-form-input>
+				<b-form-input
+					v-if="banned"
+					placeholder="You have been banned."
+					readonly
+				></b-form-input>
+				<b-input-group-append>
+					<b-button
+						@click="sendMessage"
+						v-if="!banned"
+						variant="dark"
+					>
+						Send Message
+					</b-button>
+				</b-input-group-append>
+			</b-input-group>
+			<br />
 		</b-card>
 	</div>
 </template>
@@ -40,7 +57,11 @@ import { mapState, mapActions } from "vuex";
 
 export default {
 	name: "",
-	data: () => ({ message: "" }),
+	data: () => ({
+		message: "",
+		banned: false,
+		recentMessages: [],
+	}),
 	sockets: {
 		receiveMessage(data) {
 			this.socket_updateChat(data);
@@ -49,26 +70,48 @@ export default {
 	methods: {
 		...mapActions(["socket_updateChat"]),
 		sendMessage() {
-			this.$socket.client.emit("sendMessage", {
-				roomID: this.roomID,
-				nickname: this.nickname,
-				msg: this.message,
-			}); // by who
-			this.message = "";
+			if (this.message !== "") {
+				this.$socket.client.emit("sendMessage", {
+					roomID: this.roomID,
+					nickname: this.nickname,
+					msg: this.message,
+				}); // by who
+				this.recentMessages.push(this.message);
+				this.message = "";
+			}
 		},
-		scrollToEnd(){
+		scrollToEnd() {
 			var container = document.querySelector(".scroll");
 			var scrollHeight = container.scrollHeight;
 			container.scrollTop = scrollHeight;
 		},
-		profMessage(word) {
-			return word.slice(1)
+		systemMessage(word) {
+			return word.slice(1);
 		},
 	},
-	mounted(){
+	mounted() {
 		this.scrollToEnd();
+		// banning
+		let counter = 0;
+		setInterval(() => {
+			if (this.recentMessages.length > 2) {
+				this.banned = true;
+				this.$socket.client.emit("sendMessage", {
+					roomID: this.roomID,
+					nickname: '',
+					msg: `${this.nickname} has been banned for 30 seconds for spamming.`,
+				});
+			} else {
+				counter += 1;
+				if (counter == 15) {
+					this.banned = false;
+					counter = 0;
+				}
+			}
+			this.recentMessages = [];
+		}, 2000);
 	},
-	updated(){
+	updated() {
 		this.scrollToEnd();
 	},
 	computed: {
@@ -83,7 +126,7 @@ h1 {
 	font-family: Arial, Helvetica, sans-serif;
 	font-weight: bold;
 }
-h2{
+h2 {
 	margin-top: 10px;
 	font-family: Arial, Helvetica, sans-serif;
 	/* font-weight: bold; */
@@ -98,15 +141,17 @@ h2{
 	margin: auto;
 	align-content: left;
 }
-.scroll{
-	margin:auto;
-	width:90%;
-	max-height:150px;
-	overflow:scroll;
-	margin-bottom: 20px;
-	
+#banned {
+	color: red;
 }
-.profword{
-	color:red;
+.scroll {
+	margin: auto;
+	width: 90%;
+	max-height: 150px;
+	overflow: scroll;
+	margin-bottom: 20px;
+}
+.profword {
+	color: red;
 }
 </style>
